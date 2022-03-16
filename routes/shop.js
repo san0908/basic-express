e2beeb007f6d34c3e83306faa52988a73184f94f
san0908 <sandeep.5678711@gmail.com>
2022-03-16 23:54:48 +0530

    Serving Files

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,12 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.get('/', (req, res, next) => {
     // res.send('<h1>Hello From Express.js</h1>');
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
